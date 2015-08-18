8cf0581aecc3a386aed0c2a9d5da7a0c49c82da0
CntChen <cntchen@gmail.com>
2015-08-18 12:12:49 +0800

    use UTC time

--- node-test/test.js
+++ node-test/test.js
@@ -1,6 +1,6 @@
 var timePicker = require('../src/timePicker.js').timePicker;
 
-var testArr = ['all','now','-1s@now','-m@now','-1h@now','-1d@now', '-1w@mow', '-1mon@mow','-1y@now'];
+var testArr = ['all','now','-1s@now','-1m@now','-1h@now','-1d@now', '-1w@mow', '-1mon@mow','-1y@now'];
 
 testArr.forEach(function(str){
 	console.log('\nstr: ' + str,
--- src/timePicker.js
+++ src/timePicker.js
@@ -1,76 +1,101 @@
 var exports = exports || {};
 
 var timePicker = timePicker || {};
-(function (){
+(function() {
   var SEPARATOR = '@';
-  var timeArr = ['y','mon','d','w','h','m','s'];
-  var TimePickerRexExp = /(\-?\d+(y|mon|d|w|h|m|s))?@?(all|now|y|mon|d|w|h|m|s)/;
+  var timeArr = ['y', 'mon', 'd', 'w', 'h', 'm', 's'];
+  var TimePickRexExp = /(\-?\d+(y|mon|d|w|h|m|s))?@?(all|now|y|mon|d|w|h|m|s)/;
   var defaultTimeStamp = -1;
 
-  function BeYoungAgain(timeRuleStr){
+  function BeYoungAgain(timeRuleStr) {
     timeRuleStr = timeRuleStr.toLowerCase();
 
-    if(!TimePickerRexExp.test(timeRuleStr)){
+    if (!TimePickRexExp.test(timeRuleStr)) {
       return defaultTimeStamp;
     }
 
     var timeBackNum,
-        timeBackUnit,
-        timeStoneUnit;
+      timeBackUnit,
+      timeStoneUnit;
     // 分词器
-    if(timeRuleStr.indexOf(SEPARATOR) <= 0){// 没有时间倒退值和分隔符 或 没有时间倒退值
-        timeStoneUnit = timeRuleStr.replace(SEPARATOR, '');
-    }
-    else {
+    if (timeRuleStr.indexOf(SEPARATOR) <= 0) { // 没有时间倒退值和分隔符 // 没有时间倒退值
+      timeStoneUnit = timeRuleStr.replace(SEPARATOR, '');
+    } else {
       timeBackNum = parseInt(timeRuleStr.split(SEPARATOR)[0].replace(/^\-?/, '-').replace(/(y|mon|d|w|h|m|s)$/, ''));
       timeBackUnit = timeRuleStr.split(SEPARATOR)[0].replace(/^\-?\d+/, '');
       timeStoneUnit = timeRuleStr.split(SEPARATOR)[1];
     }
-    
-    if('all' === timeStoneUnit){
+
+    if ('all' === timeStoneUnit) {
       return defaultTimeStamp;
     }
 
-    if(null === timeBackNum && null === timeBackUnit){
-       return getTimeStoneDate(timeStoneUnit).getTime();
-    }else{
-       return getTimeBackDate(timeBackNum, timeBackUnit, timeStoneUnit).getTime();
+    if (null === timeBackNum && null === timeBackUnit) {
+      return getTimeStoneDate(timeStoneUnit).getTime();
+    } else {
+      return getTimeBackDate(timeBackNum, timeBackUnit, timeStoneUnit).getTime();
     }
   }
 
-  function getTimeBackDate(timeBackNum, timeBackUnit, timeStoneUnit){
+  function getTimeBackDate(timeBackNum, timeBackUnit, timeStoneUnit) {
     var date = getTimeStoneDate(timeStoneUnit);
 
     var index = timeArr.indexOf(timeBackUnit);
-    index === timeArr.indexOf('y')?(function (){date.setYear(date.getFullYear() + timeBackNum);})():{};
-    index === timeArr.indexOf('mon')?(function (){date.setMonth(date.getMonth() + timeBackNum);})():{};
-    index === timeArr.indexOf('d')?(function (){date.setDate(date.getDate() + timeBackNum);})():{};
-    index === timeArr.indexOf('w')?(function (){date.setDate(date.getDate() + timeBackNum*7)})():{};
-    index === timeArr.indexOf('h')?(function (){date.setHours(date.getHours() + timeBackNum);})():{};
-    index === timeArr.indexOf('m')?(function (){date.setMinutes(date.getMinutes() + timeBackNum);})():{};
-    index === timeArr.indexOf('s')?(function (){date.setSeconds(date.getSeconds() + timeBackNum);})():{};
-
+    index === timeArr.indexOf('y') ? (function() {
+      date.setYear(date.getFullYear() + timeBackNum);
+    })() : {};
+    index === timeArr.indexOf('mon') ? (function() {
+      date.setUTCMonth(date.getUTCMonth() + timeBackNum);
+    })() : {};
+    index === timeArr.indexOf('d') ? (function() {
+      date.setUTCDate(date.getUTCDate() + timeBackNum);
+    })() : {};
+    index === timeArr.indexOf('w') ? (function() {
+      date.setUTCDate(date.getUTCDate() + timeBackNum * 7)
+    })() : {};
+    index === timeArr.indexOf('h') ? (function() {
+      date.setUTCHours(date.getUTCHours() + timeBackNum);
+    })() : {};
+    index === timeArr.indexOf('m') ? (function() {
+      date.setUTCMinutes(date.getUTCMinutes() + timeBackNum);
+    })() : {};
+    index === timeArr.indexOf('s') ? (function() {
+      date.setUTCSeconds(date.getUTCSeconds() + timeBackNum);
+    })() : {};
     return date;
   }
 
-  function getTimeStoneDate(timeStoneUnit){
+  function getTimeStoneDate(timeStoneUnit) {
     var date = new Date();
 
     var index = timeArr.indexOf(timeStoneUnit);
-    index < timeArr.indexOf('y')?(function (){index=timeArr.length})():{};
-    index < timeArr.indexOf('mon')?(function (){date.setMonth(0);})():{};
-    index < timeArr.indexOf('d')?(function (){date.setDate(1);})():{};
-    index === timeArr.indexOf('w')?(function (){date.setDate(date.getDate() - date.getDay());})():{};
-    index < timeArr.indexOf('h')?(function (){date.setHours(0);})():{};
-    index < timeArr.indexOf('m')?(function (){date.setMinutes(0);})():{};
-    index < timeArr.indexOf('s')?(function (){date.setSeconds(0);})():{};
+    index < timeArr.indexOf('y') ? (function() {
+      index = timeArr.length;
+    })() : {};
+    index < timeArr.indexOf('mon') ? (function() {
+      date.setUTCMonth(0);
+    })() : {};
+    index < timeArr.indexOf('d') ? (function() {
+      date.setUTCDate(1);
+    })() : {};
+    index === timeArr.indexOf('w') ? (function() {
+      date.setUTCDate(date.getUTCDate() - date.getUTCDay());
+    })() : {};
+    index < timeArr.indexOf('h') ? (function() {
+      date.setUTCHours(0);
+    })() : {};
+    index < timeArr.indexOf('m') ? (function() {
+      date.setUTCMinutes(0);
+    })() : {};
+    index < timeArr.indexOf('s') ? (function() {
+      date.setUTCSeconds(0);
+    })() : {};
 
     return date;
- }
-
- timePicker = {
+  }
+  timePicker = {
     getTimeStamp: BeYoungAgain
- };
+  };
 })();
 
 exports.timePicker = timePicker;
